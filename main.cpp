2e9bd6548c200ee1ba5f503c8de56937885900ea
Derrick C <derrick.chow234@gmail.com>
2020-07-25 10:52:50 -0400

    Added function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int addTwo(int num) {
+    return num + 2;
+}
+
 int main() {
-    cout << "Hello There m8";
+    cout << "Hello There m8\n";
+
+    cout << addTwo(2);
     return 0;
 }
